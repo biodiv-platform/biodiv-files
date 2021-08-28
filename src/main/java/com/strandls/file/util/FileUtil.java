5490cacc41253be3da89123b268bebb62ddcebe7
vishnu <teamdraftbox@gmail.com>
2021-08-28 14:12:38 +0530

    sonar bug fix fileToStream try-catch

--- src/main/java/com/strandls/file/util/FileUtil.java
+++ src/main/java/com/strandls/file/util/FileUtil.java
@@ -12,29 +12,33 @@ import javax.ws.rs.core.StreamingOutput;
 
 public class FileUtil {
 
-    private FileUtil() {
-        throw new IllegalStateException("Utility class");
-    }
+	private FileUtil() {
+		throw new IllegalStateException("Utility class");
+	}
 
-    public static Response fromFileToStream(File src, String contentType) throws IOException {
-        InputStream in = new FileInputStream(src);
-        StreamingOutput sout = new StreamingOutput() {
-            @Override
-            public void write(OutputStream out) throws IOException, WebApplicationException {
-                byte[] buf = new byte[8192];
-                int c;
-                while ((c = in.read(buf, 0, buf.length)) > 0) {
-                    out.write(buf, 0, c);
-                    out.flush();
-                }
-                out.close();
-            }
-        };
+	public static Response fromFileToStream(File src, String contentType) throws IOException {
+		InputStream in = new FileInputStream(src);
+		try {
+			StreamingOutput sout = new StreamingOutput() {
+				@Override
+				public void write(OutputStream out) throws IOException, WebApplicationException {
+					byte[] buf = new byte[8192];
+					int c;
+					while ((c = in.read(buf, 0, buf.length)) > 0) {
+						out.write(buf, 0, c);
+						out.flush();
+					}
+					out.close();
+				}
+			};
 
-        return Response.ok(sout)
-                .type(contentType)
-                .header("Content-Length", src.length())
-                .cacheControl(AppUtil.getCacheControl())
-                .build();
-    }
+			return Response.ok(sout).type(contentType).header("Content-Length", src.length())
+					.cacheControl(AppUtil.getCacheControl()).build();
+		} catch (Exception e) {
+			return Response.serverError().entity(e.getMessage()).build();
+
+		} finally {
+			in.close();
+		}
+	}
 }
